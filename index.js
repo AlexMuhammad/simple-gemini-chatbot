fdeb2fce10a325d5842b546bf5b0ead3b767b259
AlexMuhammad <saputraalex872@gmail.com>
2025-02-05 18:22:56 +0700

    fix: stream text generate

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const MAX_HISTORY_LENGTH = 10;
 const initialPrompt = `Kamu adalah chatbot PDKT yang membantu orang dalam percintaan. Berikan saran yang ramah, lucu, dan tidak menggurui.  Sapa pengguna dengan hangat.`;
 
 async function main() {
-  const chatHistory = [["user", initialPrompt]];
-  const { response } = await model.startChat().sendMessage(initialPrompt);
-  chatHistory.push(["model", response.text()]);
-
   console.log(color.bold.red("Selamat Datang Wahai Pejuang Cinta!"));
   console.log(color.bold.green("Mulai Obrolanmu"));
+  const chatHistory = [["user", initialPrompt]];
+  const response = await model.startChat().sendMessageStream(initialPrompt);
+
+  // Initial chat
+  process.stdout.write(color.bold.green("😇Dukun Cinta: "));
+  let initialResponse = "";
+  for await (const chunk of response.stream) {
+    const chunkText = chunk.text();
+    process.stdout.write(chunkText);
+    initialResponse += chunkText;
+  }
+  console.log(); // New line after response
+
+  chatHistory.push(["model", initialResponse]);
 
   while (true) {
     const prompt = readline.question(color.yellow("🤡Kamu: "));
@@ -35,28 +45,24 @@ async function main() {
         );
         return;
       }
-      // Mendapatkan respons dari model
-      const { response } = await chat.sendMessage(prompt);
-      const candidates = response.candidates;
+      // get response from chat
+      let result = await chat.sendMessageStream(prompt);
+      let fullResponse = "";
+      process.stdout.write(color.bold.green("😇Dukun Cinta: "));
 
-      if (candidates && candidates.length > 0) {
-        // OPSI: Pilih respons pertama dari candidates
-        const resultResponse = candidates[0].content.parts[0].text;
-        console.log(color.bold.green("😇Dukun Cinta: ") + resultResponse);
-        chatHistory.push(["user", prompt]);
-        chatHistory.push(["model", resultResponse]);
-
-        if (chatHistory.length > MAX_HISTORY_LENGTH) {
-          chatHistory.shift();
-          chatHistory.shift();
-        }
-
-        // Catatan: Bisa menambahkan logika untuk menentukan apakah respons dari AI memenuhi kriteria
-      } else {
-        console.log(
-          color.bold.red("😇Dukun Cinta: ") + "Maaf, tidak bisa ber-word2🥹."
-        );
+      for await (const chunk of result.stream) {
+        const chunkText = chunk.text();
+        process.stdout.write(chunkText);
+        fullResponse += chunkText;
       }
+
+      if (chatHistory.length > MAX_HISTORY_LENGTH) {
+        chatHistory.shift();
+        chatHistory.shift();
+      }
+
+      chatHistory.push(["user", prompt]);
+      chatHistory.push(["model", fullResponse]);
     } catch (error) {
       console.log(color.bold.red(error));
     }
